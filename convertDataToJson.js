5b4482d35cbd3fbeccba53b5c9666c1710bd6383
Thushar12E45 <thushar.work@gmail.com>
2021-08-20 13:45:47 +0530

    Initial Commit

--- convertDataToJson.js
+++ convertDataToJson.js
@@ -1,16 +1,27 @@
 const fs = require('fs');
 const csv = require('csvtojson');
 
-async function start() {
-  const data = await csv().fromFile('./src/inputData/covid_19_india.csv');
-  const covidData = JSON.stringify(data);
-  fs.writeFile('./public/covidData.json', covidData, function (err) {
+function writeFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
     if (err) {
       console.error(err);
     } else {
       console.log('file writing complete');
     }
   });
+}
+async function start() {
+  let covidData = await csv().fromFile('./src/inputData/covid_19_india.csv');
+  covidData = JSON.stringify(covidData);
+  writeFile('./public/covidData.json', covidData);
+
+  let testingData = await csv().fromFile('./src/inputData/StatewiseTestingDetails.csv');
+  testingData = JSON.stringify(testingData);
+  writeFile('./public/testingData.json', testingData);
+
+  let vaccineData = await csv().fromFile('./src/inputData/covid_vaccine_statewise.csv');
+  vaccineData = JSON.stringify(vaccineData);
+  writeFile('./public/vaccineData.json', vaccineData);
 }
 
 start();
